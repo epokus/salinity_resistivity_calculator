cac128a6ab34e21b0fec0f00d535bf7b6b1a9077
epokus <67133954+epokus@users.noreply.github.com>
2021-08-05 17:16:42 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from bokeh.plotting import figure
 from bokeh.io import curdoc
-from bokeh.models import ColumnDataSource, Row, Column, ranges, LinearAxis, TextInput, Select, Panel, Tabs, Button, Div
+from bokeh.models import ColumnDataSource, Row, Column, ranges, LinearAxis, TextInput, Select, Panel, Tabs, Button, Div, TableColumn, DataTable
 import os
 import pytz
 from datetime import datetime
@@ -19,34 +19,9 @@ def SAL(temp, rw):
 def FtoC(degF):
     return (degF - 32) * 5/9
 
-def calc_rw_sal(temp, sal, temp_type):
-    if temp_type == 'degC':
-        temp = FtoC(temp)
+def CtoF(degC):
+    return (degC* 9 / 5) + 32
 
-    data_plot = {'temp': [temp],
-                 'sal': [sal],
-                 'rw': [RW(temp, sal)],
-                 'text': [f'Rw: {round(RW(temp, sal), 2)}. temp: {round(temp, 2)} degF. Salinity: {round(sal, 2)}. '], }
-
-    data_plot_line = {'temps': np.arange(50, 400, 5),
-                      'rws'  : RW(np.arange(50, 400, 5), sal)}
-
-    return data_plot, data_plot_line
-
-
-def calc_sal_rw(temp, rw, temp_type):
-    if temp_type == 'degC':
-        temp = FtoC(temp)
-
-    data_plot = {'temp': [temp],
-                 'sal':  [SAL(temp, rw)],
-                 'rw':   [rw],
-                 'text': [f'Rw: {round(rw, 2)}. temp: {round(temp, 2)} degF. Salinity: {round(SAL(temp, rw), 2)}. '], }
-
-    data_plot_line = {'temps': np.arange(50, 400, 5),
-                      'rws'  : RW(np.arange(50, 400, 5), SAL(temp, rw))}
-
-    return data_plot, data_plot_line
 
 def record_data(mode, temp, rw, sal, date):
     if 'record.csv' in os.listdir():
@@ -55,6 +30,18 @@ def record_data(mode, temp, rw, sal, date):
         f = open('record.csv','w+')
     f.write(f'{mode},{temp},{rw},{sal},{date}\n')
     f.close()
+
+def salinity_line(data_plot):
+    sals = data_plot.data['sal']
+    data_plot_line = {'temps':[], 'rws':[]}
+
+    for sal in sals:
+        temp = np.arange(50, 400, 5)
+        rw   = RW(temp, sal)
+        data_plot_line['temps'].append(list(temp))
+        data_plot_line['rws'].append(list(rw))
+    return data_plot_line
+
 
 salinities = np.array([200, 300, 400, 500, 600, 700, 800, 1000, 1200, 1400, 1700, 2000, 3000, 4000, 5000, 6000, 7000,
                        8000, 10000, 12000, 14000, 17000, 20000, 30000, 40000, 50000, 60000, 70000, 80000, 100000,
@@ -87,13 +74,19 @@ p.multi_line(container_x, container_y, line_alpha=0.2, line_color='orange')
 p.text(text_x, text_y, text_s,
        angle=-20,angle_units='deg', text_font_size={'value': '8px'})
 
-data_plot, data_plot_line = calc_rw_sal(120, 2500,'degF')
+data_plot = {'rw'    :[],
+             'sal'   :[],
+             'temp'  :[],
+             'text'  :[]}
+data_plot_line = {'temps': [],
+                  'rws': []}
+
 data_plot = ColumnDataSource(data_plot)
 data_plot_line = ColumnDataSource(data_plot_line)
 
 p.circle('temp', 'rw', source=data_plot)
 p.text('temp', 'rw', 'text',source=data_plot,text_font_size={'value': '8px'})
-p.line('temps', 'rws', source=data_plot_line)
+p.multi_line('temps', 'rws', source=data_plot_line)
 
 err_msg = Div(text='')
 headings = Div(text='''<table style="height: 18px; width: 901px; border-collapse: collapse;" border="0">
@@ -122,14 +115,21 @@ tem_type  = Select(title="Temp Unit:", value="degF", options=["degF", "degC"])
 
 calc_sal_button = Button(label='Calculate')
 calc_rw_button = Button(label='Calculate')
+del_row_button = Button(label='Delete row')
 
-rw_temp  = Column(rw_input, tem_input, tem_type, calc_sal_button)
-sal_temp = Column(sal_input, tem_input, tem_type, calc_rw_button)
+
+rw_temp  = Column(rw_input, tem_input, tem_type, calc_sal_button, del_row_button)
+sal_temp = Column(sal_input, tem_input, tem_type, calc_rw_button, del_row_button)
 
 rw_tab  = Panel(child = rw_temp, title='Sal form RW@temp')
 sal_tab = Panel(child = sal_temp, title='Rw@temp form Sal')
 
 tabs = Tabs(tabs=[rw_tab, sal_tab])
+
+columns = [TableColumn(field='rw', title='Rw (ohmm)'),
+           TableColumn(field='sal', title='Salinity (NaCl ppm)'),
+           TableColumn(field='temp', title='Temp'),]
+table = DataTable(source=data_plot, columns=columns, editable= True, width=350)
 
 def calc_sal_button_callback():
     try:
@@ -137,43 +137,87 @@ def calc_sal_button_callback():
         datetime_tz = datetime.now(tz).strftime("%D %H:%M:%S")
         temp = float(tem_input.value)
         rw   = float(rw_input.value)
-        type = tem_type.value
+        tem_unit = tem_type.value
+        if tem_unit == 'degC':
+            temp = FtoC(temp)
+        sal = round(SAL(temp, rw),2 )
 
-        temp1, temp2 = calc_sal_rw(temp, rw, type)
-        data_plot.data      = temp1
-        data_plot_line.data = temp2
-        err_msg.text = ''''''
-        record_data('Rw to get Sal', temp, 0, rw, datetime_tz)
+        data = data_plot.data.copy()
+        data['temp'].append(temp)
+        data['rw'].append(rw)
+        data['sal'].append(sal)
+        data['text'].append([f'Rw: {rw}. temp: {temp} degF. Salinity: {sal}. '])
+
+        data_plot.data = dict(data)
+        data_plot_line.data = dict(salinity_line(data_plot))
+
+        record_data(tem_unit, temp, rw, sal, datetime_tz)
 
     except:
         err_msg.text = '''<pre>!!! temp, sal, and rw must be a number</pre>'''
-
-
-    # print('hello', temp, rw, type)
 
 def calc_rw_button_callback():
     try:
         tz = pytz.timezone('Asia/Jakarta', )
         datetime_tz = datetime.now(tz).strftime("%D %H:%M:%S")
-
-        temp  = float(tem_input.value)
+        temp = float(tem_input.value)
         sal   = float(sal_input.value)
-        type  = tem_type.value
-
-        temp1, temp2 = calc_rw_sal(temp, sal, type)
-        data_plot.data      = temp1
-        data_plot_line.data = temp2
-        err_msg.text = ''''''
-        record_data('Sal to get Rw', temp, sal, 0, datetime_tz)
+        tem_unit = tem_type.value
+        if tem_unit == 'degC':
+            temp = FtoC(temp)
+        rw = round(RW(temp, sal), 2)
+        data = data_plot.data.copy()
+        data['temp'].append(temp)
+        data['rw'].append(rw)
+        data['sal'].append(sal)
+        data['text'].append([f'Rw: {rw}. temp: {temp} degF. Salinity: {sal}. '])
+        data_plot.data = dict(data)
+        data_plot_line.data = dict(salinity_line(data_plot))
+        record_data(tem_unit, temp, rw, sal, datetime_tz)
 
     except:
         err_msg.text = '''<pre>!!! temp, sal, and rw must be a number</pre>'''
 
-    # print('hello 2', temp, sal, type)
+def del_row_callback():
+    try:
+        selection = data_plot.selected.indices[0]
+        print('yow',selection)
+        data = data_plot.data.copy()
+
+        rw   = data['rw']
+        sal  = data['sal']
+        temp = data['temp']
+        text = data['text']
+
+        rw.pop(selection)
+        sal.pop(selection)
+        temp.pop(selection)
+        text.pop(selection)
+
+        out = {'rw': rw,
+               'sal':sal,
+               'temp':temp,
+               'text':text}
+
+
+        data_plot.data = out
+
+    except:
+        selection = data_plot.selected.indices
+        data = dict(data_plot.data)
+        print('except',selection)
+
+        data['rw'] = data['rw'][:-1]
+        data['sal'] = data['sal'][:-1]
+        data['temp'] = data['temp'][:-1]
+        data['text'] = data['text'][:-1]
+        data_plot.data = data
+
 
 calc_sal_button.on_click(calc_sal_button_callback)
 calc_rw_button.on_click(calc_rw_button_callback)
+del_row_button.on_click(del_row_callback)
 
-stack = Column(headings, Row(p, Column(tabs, err_msg)),footer)
+stack = Column(headings, Row(p, Column(tabs, err_msg), table),footer)
 curdoc().add_root(stack)
 curdoc().title='Salinity Calculator - SPWLA'
